f61df9ebf70e4b0f453d83dc5461afa657e9e9e6
landpack <ada.frank.ak@gmail.com>
2016-07-23 15:31:33 +0800

    api is work fine

---
+++ just_pt/demo/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from demo.models import Message, UserMessage
+from demo.serializers import MessageSerializer, UserMessageSerializer
+
+class JSONResponse(HttpResponse):
+	"""
+	An HttpResponse that renders its content into JSON.
+	"""
+	def __init__(self, data, **kwargs):
+		content = JSONRenderer().render(data)
+		kwargs['content_type'] = 'application/json'
+		super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def message_list(request):
+	"""
+	List all message, or create a new message
+	"""
+	
+	if request.method == 'GET':
+		message = Message.objects.all()
+		serializer = MessageSerializer(message, many=True)
+		return JSONResponse(serializer.data)
+
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = MessageSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JSONResponse(serializer.data, status=201)
+		return JSONResponse(serializer.errors, status=400)
+
+
+
